6b8630fc5ef79bcee5ac01c3b290a632c6507424
Olga <olga@MacBook-Pro-Olga-2.local>
2020-10-22 22:55:46 +0200

    01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,15 @@
+/**
+ * @param a {Number}
+ * @param b {Number}
+ * @return {Number}
+ */
+
 function sum(a, b) {
-  /* ваш код */
+    if (typeof a === "number" && typeof b === "number") {
+        return a + b
+    } else {
+        throw TypeError ("функция должна бросать ошибку TypeError")
+    }
 }
 
 module.exports = sum;
